3580198ebc64d4b9e3873de1abd54cd1ebe6e54a
David Goodwin <goodwid@gmail.com>
2017-01-27 18:03:52 -0800

    Notes added

--- code/thursday.js
+++ code/thursday.js
@@ -18,11 +18,11 @@ if (day === 'Thursday') {
 
 // TODO: After commenting out the 3 conditionals above,
 //       uncomment this code and run it.
-// if (day === 'Thursday') {
-//   console.log('Is it Friday yet?');
-// } else {
-//   console.log('Whistle while you work!')
-// }
+if (day === 'Thursday') {
+  console.log('Is it Friday yet?');
+} else {
+  console.log('Whistle while you work!')
+}
 
 // TODO: Add an "else if" block to check whether it's Saturday
 //       and console.log() a different statement for the weekend.
@@ -54,9 +54,9 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // console.log('index 20: ' + days[20]);
 
 // TODO: DRY it up!
-// for (var i = 0; i < 7; i++) {
-//   console.log('index ' + i + ': ' + days[i]);
-// }
+for (var i = 0; i < days.length; i++) {
+  console.log('index ' + i + ': ' + days[i]);
+}
 
 
 // TODO: Replace the "7" in "i < 7" with the length of the array
@@ -65,8 +65,8 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // TODO: On each iteration, use our randomly generated dayNumber
 //       to get a day from the days array, and console.log the
 //       name of the day.
-// var max = 100,
-//     result;
-// for (var j = 0; j < max; j++) {
-//   dayNumber = Math.floor(Math.random() * days.length);
-// }
+var max = 100,
+    result;
+for (var j = 0; j < max; j++) {
+  dayNumber = Math.floor(Math.random() * days.length);
+}
---
+++ notes/thursday.md
@@ -0,0 +1,5 @@
+3 steps to writing code:
+
+1. Make it run
+1. Make it right
+1. Make it fast
